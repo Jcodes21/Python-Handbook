97b3b29fcac09d8d83b6738ab5e78a369ce12ca0
Joseph halden <josephhalden1@gmail.com>
2025-10-16 19:32:37 +0100

    Added data type examples file

---
+++ data_type_examples.py
@@ -0,0 +1,30 @@
+# Numeric Data types
+integer = 2025
+float = 2.25
+complex_number = 3j
+
+print(type(integer))
+print(type(float))
+print(type(complex_number))
+
+# Text Data types
+string = 'String'
+string2 = "String"
+string_number = '2025'
+
+print(type(string))
+print(type(string2))
+print(type(string_number))
+
+# Boolean Data types
+isBool = True
+print(type(isBool))
+
+# Sequence Data types
+list = ["Apple", "Orange", "Cherry", 2, True]
+tuple = ("Apple", "Orange", "Cherry", 2, True)
+
+print(list)
+print(tuple)
+
+
